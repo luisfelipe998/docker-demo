8d439096068f644e27dd27be28aba5edb3bed558
Luis <luis.borsoi@sap.com>
2022-06-10 23:32:15 -0300

    add network in k8s install script

--- k8s_install.sh
+++ k8s_install.sh
@@ -3,7 +3,9 @@ kubectl delete service app-udp
 kubectl delete deployment mongodb
 kubectl delete service mongodb
 kubectl delete pvc db
+kubectl delete networkpolicy network
 
+kubectl apply -f ./k8s/network-networkpolicy.yaml
 kubectl apply -f ./k8s/mongodb-deployment.yaml
 kubectl apply -f ./k8s/mongodb-service.yaml
 kubectl apply -f ./k8s/db-persistentvolumeclaim.yaml
